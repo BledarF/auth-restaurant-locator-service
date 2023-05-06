b5f49b473ae48e3f104a946eada2c3d7c5f9cdba
BledarFeratiEY <104501743+BledarFeratiEY@users.noreply.github.com>
2023-05-06 18:16:11 +0100

    Add necessary maven project pre-build

---
+++ src/main/java/auth/authrestaurantlocator/AuthRestaurantLocatorApplication.java
@@ -0,0 +1,13 @@
+package auth.authrestaurantlocator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthRestaurantLocatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthRestaurantLocatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/auth/authrestaurantlocator/AuthRestaurantLocatorApplicationTests.java
@@ -0,0 +1,13 @@
+package auth.authrestaurantlocator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthRestaurantLocatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
